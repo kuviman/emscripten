deb8313e60f629d050758bb29520b85ec57b5d9f
kuviman <kuviman@gmail.com>
2017-11-18 08:38:50 +0400

    Restructuring + renaming + web-launcher

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+extern crate core;
+
+extern "C" {
+    pub fn emscripten_GetProcAddress(
+        name: *const std::os::raw::c_char,
+    ) -> *const std::os::raw::c_void;
+}
+
+include!("gen.rs");
