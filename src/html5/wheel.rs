0a98bde68290baa54823a8d8d9af63aec852c8ff
kuviman <kuviman@gmail.com>
2017-11-22 12:22:34 +0400

    emscripten (safe binding) crate

--- src/html5/wheel.rs
+++ src/html5/wheel.rs
@@ -2,11 +2,11 @@ use ::*;
 
 pub const EMSCRIPTEN_EVENT_WHEEL: EM_EVENT_TYPE = 9;
 
-pub type EM_DELTA_MODE = c_ulong;
+pub type DOM_DELTA_MODE = c_ulong;
 
-pub const DOM_DELTA_PIXEL: EM_DELTA_MODE = 0x00;
-pub const DOM_DELTA_LINE: EM_DELTA_MODE = 0x01;
-pub const DOM_DELTA_PAGE: EM_DELTA_MODE = 0x02;
+pub const DOM_DELTA_PIXEL: DOM_DELTA_MODE = 0x00;
+pub const DOM_DELTA_LINE: DOM_DELTA_MODE = 0x01;
+pub const DOM_DELTA_PAGE: DOM_DELTA_MODE = 0x02;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -15,7 +15,7 @@ pub struct EmscriptenWheelEvent {
     pub deltaX: c_double,
     pub deltaY: c_double,
     pub deltaZ: c_double,
-    pub deltaMode: EM_DELTA_MODE,
+    pub deltaMode: DOM_DELTA_MODE,
 }
 
 pub type em_wheel_callback_func = Option<unsafe extern "C" fn(
