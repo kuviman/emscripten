9391c0039123f248a48d3fddf12d94321caec11e
kuviman <kuviman@gmail.com>
2017-11-22 18:36:59 +0400

    emscripten::run_script functions are actually safe

--- src/emscripten/call_js.rs
+++ src/emscripten/call_js.rs
@@ -1,18 +1,18 @@
 use ::*;
 
-pub unsafe fn run_script(script: &str) {
+pub fn run_script(script: &str) {
     let script = CString::new(script).unwrap();
-    emscripten_run_script(script.as_ptr());
+    unsafe { emscripten_run_script(script.as_ptr()); }
 }
 
-pub unsafe fn run_script_i32(script: &str) -> i32 {
+pub fn run_script_i32(script: &str) -> i32 {
     let script = CString::new(script).unwrap();
-    emscripten_run_script_int(script.as_ptr()) as i32
+    unsafe { emscripten_run_script_int(script.as_ptr()) as i32 }
 }
 
-pub unsafe fn run_script_string(script: &str) -> String {
+pub fn run_script_string(script: &str) -> String {
     let script = CString::new(script).unwrap();
-    let string = {
+    let string = unsafe {
         let string = emscripten_run_script_string(script.as_ptr());
         CStr::from_ptr(string)
     };
