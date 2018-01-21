2f86a1d859842576731654ce28f03ec77947cc23
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-21 19:20:41 +0000

    Deploy kuviman/emscripten to github.com/kuviman/emscripten.git:gh-pages

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["emscripten"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"emscripten/webgl/struct.ContextAttributes.html\" title=\"struct emscripten::webgl::ContextAttributes\">ContextAttributes</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
